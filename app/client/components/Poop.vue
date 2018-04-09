95fadd2e43e58877fbcfdeb470207eef53211a53
Josh Thompson <josh3276@gmail.com>
2018-04-09 15:36:41 +0200

    🐮 test auto git update

--- app/client/components/Poop.vue
+++ app/client/components/Poop.vue
@@ -27,7 +27,7 @@
 		<div class="name">{{name}}</div>
 		<div class="icon" :style="`font-size: ${size}px`">
 			<span v-if="errors !== 0">💩</span>
-			<span v-if="errors === 0">🚽</span>
+			<span v-if="errors === 0">🐮</span>
 		 </div>
 		<div class="errors">{{errors}}</div>
 	</div>
